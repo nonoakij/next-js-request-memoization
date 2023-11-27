bb8b6bbce4f946178c5fda845f8e773e637485fd
nonoakij <nonoakij@gmail.com>
2023-11-27 15:35:15 +0900

    init

---
+++ packages/next/app/page.tsx
@@ -0,0 +1,23 @@
+export const dynamic = "force-dynamic";
+
+export default async function Home() {
+  return (
+    <main>
+      <h1>Hello world</h1>
+      <RequestAPIComponent />
+      <RequestAPIComponent />
+      <RequestAPIComponent />
+      <RequestAPIComponent />
+      <RequestAPIComponent />
+      <RequestAPIComponent />
+      <RequestAPIComponent />
+      <RequestAPIComponent />
+    </main>
+  );
+}
+
+async function RequestAPIComponent() {
+  const res = await fetch("http://localhost:8787/");
+  const data = await res.json();
+  return <div>{data.text}</div>;
+}
